33db0a72327c5a7a3fc7fcbd821440f3c93c0029
Dysio <td.dyszewski@gmail.com>
2022-05-17 10:42:27 +0200

    merged api.py with dtcli-improvements

--- dtcli/api.py
+++ dtcli/api.py
@@ -1,10 +1,7 @@
 import json
 import os
 import requests as _requests_impl
-<<<<<<< HEAD
-=======
 import zipfile, io
->>>>>>> APM-314207-dtcli-improvements
 
 # TODO: support pagination
 
@@ -14,34 +11,12 @@ class DynatraceAPIClient:
         self.headers = {"Authorization": f"Api-Token {token}"}
         self.requests = requests if requests is not None else _requests_impl
 
-<<<<<<< HEAD
-    @classmethod
-    def from_dev_tenant(cls, tenant, token):
-        tenant_url = f"https://{tenant}.dev.dynatracelabs.com"
-        return cls(tenant_url, token)
-
-    @classmethod
-    def from_tenant_url(cls, _tenant_url, token):
-        tenant_url = f"{_tenant_url}"
-        return cls(tenant_url, token)
-
-    def acquire_extension_alert(self, alert_id: str):
-        r = self.requests.get(self.url_base + f"/api/config/v1/anomalyDetection/metricEvents/" + alert_id, headers=self.headers)
-        r.raise_for_status()
-        return r.json()
-
-=======
->>>>>>> APM-314207-dtcli-improvements
     def acquire_monitoring_configurations(self, fqdn: str):
         r = self.requests.get(self.url_base + f"/api/v2/extensions/{fqdn}/monitoringConfigurations", headers=self.headers)
         r.raise_for_status()
         return r.json()["items"]
 
     def acquire_environment_configuration(self, fqdn: str):
-<<<<<<< HEAD
-        # TODO: check the url if that 100% it
-=======
->>>>>>> APM-314207-dtcli-improvements
         r = self.requests.get(self.url_base + f"/api/v2/extensions/{fqdn}/environmentConfiguration", headers=self.headers)
 
         if r.status_code == 404:
@@ -51,19 +26,11 @@ class DynatraceAPIClient:
         return r.json()
 
     def acquire_extensions(self):
-<<<<<<< HEAD
-        # TODO: check the url if that 100% it
-=======
->>>>>>> APM-314207-dtcli-improvements
         r = self.requests.get(self.url_base + f"/api/v2/extensions", headers=self.headers)
         r.raise_for_status()
         return r.json()["extensions"]
 
     def acquire_extension_versions(self, fqdn: str):
-<<<<<<< HEAD
-        # TODO: check the url if that 100% it
-=======
->>>>>>> APM-314207-dtcli-improvements
         r = self.requests.get(self.url_base + f"/api/v2/extensions/{fqdn}", headers=self.headers)
 
         r.raise_for_status()
@@ -114,12 +81,7 @@ class DynatraceAPIClient:
         if matches:
             return matches[0]
 
-<<<<<<< HEAD
-        print(f"Target version {target_version} does not exist. \nAvailable versions: {versions}")
-        raise SystemExit
-=======
         raise SystemExit(f"Target version {target_version} does not exist. \nAvailable versions: {versions}")
->>>>>>> APM-314207-dtcli-improvements
 
     def download_schemas(self, target_version: str, download_dir: str):
         """Downloads schemas from choosen version"""
@@ -128,21 +90,6 @@ class DynatraceAPIClient:
 
         if not os.path.exists(download_dir):
             os.makedirs(download_dir)
-<<<<<<< HEAD
-            print("Directory for schemas created")
-
-        print(f"Downloading schemas for version {version}")
-
-        files = self.requests.get(self.url_base + f"/api/v2/extensions/schemas/{version}", headers=self.headers).json().get("files", [])
-        for file in files:
-            schema = self.requests.get(self.url_base + f"/api/v2/extensions/schemas/{version}/{file}", headers=self.headers).json()
-            with open(file=f"{download_dir}/{file}", mode="w") as f:
-                json.dump(schema, f, indent=2)
-
-        print("Finished")
-
-        return 0
-=======
 
         header = self.headers
         header["accept"] = "application/octet-stream"
@@ -174,7 +121,6 @@ class DynatraceAPIClient:
         zfile.close()
 
         return version
->>>>>>> APM-314207-dtcli-improvements
 
     def point_environment_configuration_to(self, fqdn: str, version: str):
         r = self.requests.put(self.url_base + f"/api/v2/extensions/{fqdn}/environmentConfiguration", headers=self.headers, json={"version": version})
