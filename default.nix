f0d078473041b117248cf0b484699738c29d7246
Tomasz Gryzoń <olixem@gmail.com>
2022-06-28 13:58:54 +0200

    Add useful development amenities (#74)
    * Add useful development amenities
    
    Pinning the Python with nix sets the ground for reproducability,
    Makefile allows for having self-documenting code, instead of a README
    and finally direnv and the tiny script is just plain nice.
    
    You can for example have two windows open side-by-side and copy-paste
    commands between an environment that has a dt-cli install and a feature
    under development. It also abstracts away poetry.

---
+++ default.nix
@@ -0,0 +1,30 @@
+let
+  nixpkgs = builtins.fetchGit {
+    url = "https://github.com/nixos/nixpkgs/";
+    ref = "refs/heads/nixos-unstable";
+    rev = "f2537a505d45c31fe5d9c27ea9829b6f4c4e6ac5"; # 27-06-2022
+    # obtain via `git ls-remote https://github.com/nixos/nixpkgs nixos-unstable`
+  };
+  pkgs = import nixpkgs { config = {}; };
+  pythonCore = pkgs.python39;
+  myPython = pythonCore.withPackages pythonPkgs;
+  pythonPkgs = python-packages: with python-packages; [
+      poetry
+    ];
+  env = pkgs.buildEnv {
+    name = "dtcli-dev-env";
+    paths =
+    with pkgs;
+    [
+      git
+      gnumake
+      myPython
+      entr
+    ];
+  };
+in
+{
+  shell = pkgs.mkShell {
+    buildInputs = [ env ];
+  };
+}
