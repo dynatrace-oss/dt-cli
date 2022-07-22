8e2a6c08869f0d18e1e19041fe63b4502d2e18a6
Olgierd "Allgreed" Kasprowicz <olgierd.kasprowicz@dynatrace.com>
2022-07-22 13:47:11 +0200

    [EXT-276] Create a deprecation decorator

---
+++ dtcli/click_helpers.py
@@ -0,0 +1,56 @@
+import functools
+import types
+from typing import Callable, Any, TypeVar, Optional
+
+import click
+
+
+T = TypeVar("T")
+U = TypeVar("U")
+
+
+def mk_click_callback(f: Callable[[T], U]) -> Callable[[Any, Any, T], U]:
+    @functools.wraps(f)
+    def wrapper(_, __, v):
+        return f(v)
+    return wrapper
+
+
+# TODO: type the returns
+def _deprecated_above(deprecation_warning: str):
+    def decorator(f):
+        assert isinstance(f, click.core.Command), "decorator is placed above @click.command, therefore decorating a click Command, instead of a bare function"
+        command: click.core.Command = f
+
+        command.short_help = "[deprecated]"
+        command.hidden = True
+        command.deprecated = True
+
+        command.help = f"{deprecation_warning}\n{command.help}"
+    return decorator
+
+
+def _deprecated_below(warning_f: Callable[[], None]):
+    def decorator(f):
+        assert isinstance(f, types.FunctionType), "decorator is placed below @click.command, therefore decorating a a bare function, not a registered Click command"
+
+        @functools.wraps(f)
+        def wrapper(*args, **kwargs):
+            warning_f()
+            return f(*args, **kwargs)
+        return wrapper
+    return decorator
+
+
+def deprecated(alternative: Optional[str], alternative_help: Optional[str] = None):
+    """
+    This has to happen this way.
+
+    Click decorator registers the function automatically, so I'd need to track where it's registered or something [so the acutal function that's run could be decorated], instead I've opted for spawining 2 decorators and just doing it the hacky way.
+    """
+    if alternative:
+        alt_text = f"\nPlease consider using {click.style(alternative,fg='bright_cyan')} instead.{' ' + alternative_help.capitalize() + '.' if alternative_help else ''}\n"
+    else:
+        alt_text = ""
+    warning = f"{click.style('This function is deprecated', fg='red')}.{alt_text}"
+    return (_deprecated_above(warning), _deprecated_below(lambda: click.echo(warning)))
