6d72488d3f218f6efbc3003591a340ae1a40d7da
Olgierd Kasprowicz <olgierd.kasprowicz@dynatrace.com>
2022-09-21 16:50:04 +0200

    Bump lock and default.nix

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@ let
   nixpkgs = builtins.fetchGit {
     url = "https://github.com/nixos/nixpkgs/";
     ref = "refs/heads/nixos-unstable";
-    rev = "f2537a505d45c31fe5d9c27ea9829b6f4c4e6ac5"; # 27-06-2022
+    rev = "f677051b8dc0b5e2a9348941c99eea8c4b0ff28f"; # 21-09-2022
     # obtain via `git ls-remote https://github.com/nixos/nixpkgs nixos-unstable`
   };
   pkgs = import nixpkgs { config = {}; };
